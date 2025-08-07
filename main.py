14555f522eebebca9771538f2c4ba5d798d3e24b
alishams11 <ali.shamsodini1111@gmail.com>
2025-08-07 10:51:37 +0330

    Add feature to save generated payloads to timestamped file in outputs

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ from core.xss import generate_xss_payloads
 from core.sqli import generate_sqli_payloads
 from core.encoder import apply_encodings
 
+import os
+from datetime import datetime
+
+def save_to_file(payloads, payload_type, param):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
+    filename = f"outputs/payloads_{payload_type}_{param}_{timestamp}.txt"
+    os.makedirs("outputs", exist_ok=True)
+    try:
+        with open(filename, "w") as f:
+            for payload in payloads:
+                if isinstance(payload, list):
+                    for p in payload:
+                        f.write(p + "\n")
+                else:
+                    f.write(payload + "\n")
+        print(f"\n[+] Payloads saved to {filename}")
+    except Exception as e:
+        print(f"[!] Error saving file: {e}")
+
+
 def main():
     parser = argparse.ArgumentParser(description="Payload Generator for XSS and SQLi")
     parser.add_argument('--type', required=True, choices=['xss', 'sqli'], help='Type of vulnerability payload (xss/sqli)')
@@ -29,6 +49,6 @@ def main():
             print("---")
         else:
             print(payload)
-
+            save_to_file(payloads, args.type, args.param)
 if __name__ == "__main__":
     main()
